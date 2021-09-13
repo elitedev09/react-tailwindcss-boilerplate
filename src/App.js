d2af6f57f6e18f708d1d3e4f949cd49a3b25a0a3
piaocheng0811 <pc61300811@gmail.com>
2021-09-13 18:34:41 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App" class="p-6 items-center justify-center">
+      <h1 className="text-blue-600">Hello World</h1>
+      <p className="tracking-widest">This is my first React App.</p>
+    </div>
+  );
+}
+
+export default App;
